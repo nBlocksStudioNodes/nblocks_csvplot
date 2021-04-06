870a63f9fc9c25bd0566f185536ae40e3e2776a2
nikos chalikias <nikos.chalikias@gmail.com>
2021-04-06 17:00:02 +0100

    node compled in program 42D_

--- csvplot.cpp
+++ csvplot.cpp
@@ -1,14 +1,15 @@
 #include "csvplot.h"
 
-nBlock_CSVplot::nBlock_CSVplot(const char * label1, const char * label2, const char * label3, const char * label4) {
-    _label1 = label1;
+nBlock_CSVplot::nBlock_CSVplot(const char * label1, const char * label2, const char * label3, const char * label4) { 
+    _label1 = label1,
     _label2 = label2;
     _label3 = label3;
     _label4 = label4;
-
-    _format = "%f,";
-    string_buffer_in[0] = 0; 
-	string_buffer_out[0] = 0; 
+    
+    string_buffer0[0] = 0; 
+    string_buffer1[0] = 0;
+    string_buffer2[0] = 0;
+    string_buffer3[0] = 0;
     outputType[0] = OUTPUT_TYPE_STRING;
 }
  
@@ -16,35 +17,36 @@ void nBlock_CSVplot::triggerInput(nBlocks_Message message){
     // Any Input triggers an output regardless of value
     // 
     if (message.inputNumber == 0) { 
-        sprintf(string_buffer0, _format, message.stringValue);
-        x[0] = strlen(string_buffer0);       
+        sprintf(string_buffer0, _label1, message.stringValue);
+        x0 = strlen(string_buffer0);       
     }
     if (message.inputNumber == 1) { 
-        sprintf(string_buffer1, _format, message.stringValue);          
-        x[1] = strlen(string_buffer1);  
+        sprintf(string_buffer1, _label2, message.stringValue);          
+        x1 = strlen(string_buffer1);  
     }
     if (message.inputNumber == 2) {
-        sprintf(string_buffer2, _format, message.stringValue);            
-        x[2] = strlen(string_buffer2);  
+        sprintf(string_buffer2, _label3, message.stringValue);            
+        x2 = strlen(string_buffer2);  
     }
     if (message.inputNumber == 3) { 
-        sprintf(string_buffer3, _format, message.stringValue);           
-        x[3] = strlen(string_buffer3);  
+        sprintf(string_buffer3, _label4, message.stringValue);           
+        x3 = strlen(string_buffer3);  
     }            
 }	
 
 void nBlock_CSVplot::endFrame(void) {
-	if (x[0] > 0) {
-		for (uint32_t i=0; i < x[0]; i++){
-			string_buffer_out[i] = string_buffer0[i];
+	if (x0 > 0) {
+		for (uint32_t i=0; i < x0; i++){
+			string_buffer0[i] = string_buffer0[i];
 		}
-	if (x[1] > 0) {
-		for (uint32_t i=x[0]; i < (x[0]+x[1]); i++){
-			string_buffer_out[i] = string_buffer0[i];
+	if (x1 > 0) {
+		for (uint32_t i=x1; i < (x0+x1); i++){
+			string_buffer1[i] = string_buffer0[i];
 		}        
 		// Output contains pointer to string
-		output[0] = (uint32_t)(&string_buffer_out);
+		output[0] = (uint32_t)(&string_buffer0);
 	}
-    x[0]=0; x[1]=0; x[2]=0; x[3]=0;
+    x0=0; x1=0; x2=0; x3=0;
     return;	
+    }
 }
--- csvplot.h
+++ csvplot.h
@@ -3,13 +3,18 @@
 
 #include "nworkbench.h"
 
-class Block_CSVplot: public nBlockSimpleNode<1> {
+class nBlock_CSVplot: public nBlockSimpleNode<1> {
 
 public:
     nBlock_CSVplot(const char * label1, const char * label2, const char * label3, const char * label4);
     void triggerInput(nBlocks_Message message);
 	void endFrame(void);
     uint32_t newValueFLAG;
+    uint32_t x0;
+    uint32_t x1;
+    uint32_t x2;
+    uint32_t x3;
+    
 
 private:	
     float _ratio1;
@@ -17,9 +22,17 @@ private:
     float _ratio3;
     float _ratio4; 
     float _outvalue;
-    char string_buffer_in[256];
-    char string_buffer_out[256];
-    const char * _format;
+    char string_buffer0[32];
+    char string_buffer1[32];
+    char string_buffer2[32];
+    char string_buffer3[32];            
+
+    const char * _label1;
+    const char * _label2;
+    const char * _label3;
+    const char * _label4;
+
+    
 
 };
  
